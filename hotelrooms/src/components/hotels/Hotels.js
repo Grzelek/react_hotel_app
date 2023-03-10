823a78f7e65d2de42bc1628dc0baf7bd85771ed0
pawel laptop <grzelek9@gmail.com>
2023-03-10 20:37:27 +0000

    components added

---
+++ hotelrooms/src/components/hotels/Hotels.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import Hotel from "./Hotel/Hotel";
+
+class Hotels extends React.Component {
+    render(){
+        return(<div>
+            hotels
+            <Hotel/>
+            </div>);
+    }
+}
+
+export default Hotels;
