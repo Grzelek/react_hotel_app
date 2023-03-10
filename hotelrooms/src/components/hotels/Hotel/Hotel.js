4b3fb8edc331a82bb5553300a1dd5d6c847cbfc4
pawel laptop <grzelek9@gmail.com>
2023-03-10 23:16:09 +0000

    css updates

--- hotelrooms/src/components/hotels/Hotel/Hotel.js
+++ hotelrooms/src/components/hotels/Hotel/Hotel.js
@@ -6,23 +6,28 @@ function Hotel(){
     return(
     <div className={`row ${styles.hotelWrap}`}>
         <div className="col-4">
-        <img className={styles.hotelImage}src={hotelImg} alt="alttext"/>
+            <img className={`img-thumbnail ${styles.hotelImage}`}src={hotelImg} alt="alttext"/>
         </div>
         <div className="col-8">
-            <p>Title</p>
-            <p>Town</p>
+            <div className="row">
+                <div className="col">
+                    <p>Title</p>
+                    <p>Town</p>
+                </div>
+                <div className="col">
+                    <p>Ocena: 8.3</p>
+                    <p>Opinie: 100</p>
+                </div>
+            </div>
         </div>
-        <div className="col-1">    
+        <div className="col-12">    
             <p>Description</p>
-            <a href="#" className="btn btn-primary">Show</a>
+            <div className="d-flex justify-content-end">
+                <a href="#" className="btn btn-primary float-right">Show</a>
+            </div>
+            
         </div>
-        <div className="col-8">
-            <p>Title</p>
-            <p>Town</p>
-            <p>Description</p>
-            <a href="#" className="btn btn-primary">Show</a>
-        </div>
-        </div>
+    </div>
     )
 }
 
