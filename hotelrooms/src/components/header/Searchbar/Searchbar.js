686669697f41bba00e31a7b6625066906e2a81c0
pawel laptop <grzelek9@gmail.com>
2023-03-11 18:18:03 +0000

    search function

--- hotelrooms/src/components/header/Searchbar/Searchbar.js
+++ hotelrooms/src/components/header/Searchbar/Searchbar.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Searchbar(){
+    const [term, setTerm] = useState('');
+
+    const search = () => {
+        console.log('szukaj', term)
+    }
+
+    
     return(
         <div className="d-flex">
             <div className="form-group">
-                <input className="form-control" style={{width: "calc(100% - 20px", marginRight: '20px'}} type="text" placeholder="szukaj...."/>
+                <input 
+                value={term}
+                onKeyDown={e=> e.key === "Enter" && search()}
+                onChange={e => setTerm(e.target.value)}
+                className="form-control" 
+                style={{width: "calc(100% - 20px", marginRight: '20px'}} 
+                type="text" 
+                placeholder="szukaj...."/>
             </div>
             <div className="">
-                <button className="btn btn-primary">Szukaj</button>
+                <button 
+                onClick={search}
+                className="btn btn-primary">Szukaj</button>
             </div>
         </div>
     )
