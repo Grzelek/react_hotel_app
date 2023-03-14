c96d05f9878b72e14b50a24edeff4da13d399d22
pawel laptop <grzelek9@gmail.com>
2023-03-14 21:09:08 +0000

    class App rewrite to function App

--- hotelrooms/src/App.js
+++ hotelrooms/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Hotels from './components/Hotels/Hotels';
 import Loader from './components/UI/Loader/Loader';
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import Searchbar from './components/UI/Searchbar/Searchbar';
 import Layout from './components/Layout/Layout';
 import Footer from './components/Footer/Footer';
@@ -12,115 +12,111 @@ import {ThemeContext} from './context/themeContext';
 import AuthContext from './context/authContext';
 
 
+const hotelsList = [
+  {
+    id:1,
+    name: 'Prime Hotel',
+    town: 'London',
+    rating: 8.2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus ullamcorper quam.',
+    image: ''
+  },
+  {
+    id:2,
+    name: 'Big Hotel',
+    town: 'Watford',
+    rating: 7.4,
+    description: 'Lorem Ipsum impsum lorem',
+    image:''
+  },
+  {
+    id:3,
+    name: 'Holiday Inn',
+    town: 'Milton Keynes',
+    rating: 4.8,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus ullamcorper quam.',
+    image:''
 
-class App extends Component {
+  },
+  {
+    id:4,
+    name: 'Knights Hotel',
+    town: 'St Albans',
+    rating: 9.6,
+    description: ' Mauris maximus mi non aliquam elementum. Pellentesque eu lorem erat. Phasellus ornare ornare libero, non volutpat sapien luctus a.',
+    image:''
+  },
+];
 
-  hotels = [
-      {
-        id:1,
-        name: 'Prime Hotel',
-        town: 'London',
-        rating: 8.2,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus ullamcorper quam.',
-        image: ''
-      },
-      {
-        id:2,
-        name: 'Big Hotel',
-        town: 'Watford',
-        rating: 7.4,
-        description: 'Lorem Ipsum impsum lorem',
-        image:''
-      },
-      {
-        id:3,
-        name: 'Holiday Inn',
-        town: 'Milton Keynes',
-        rating: 4.8,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus ullamcorper quam.',
-        image:''
-    
-      },
-      {
-        id:4,
-        name: 'Knights Hotel',
-        town: 'St Albans',
-        rating: 9.6,
-        description: ' Mauris maximus mi non aliquam elementum. Pellentesque eu lorem erat. Phasellus ornare ornare libero, non volutpat sapien luctus a.',
-        image:''
-      },
-    ];
 
-  state = {
-    hotels: [],
-    loading: true, 
-    colorTheme:'primary',
-    isAuthenticated:false
+function App(){
+
+  const [hotels, setHotels] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [theme, setTheme] = useState('primary')
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const searchHandler = (term) => {
+    const hotelsResults = [...hotelsList].filter(
+          x => x.name.toLowerCase()
+          .includes(term.toLowerCase())
+        )
+    setHotels(hotelsResults)
   }
-  
-searchHandler(term){
-  const hotels = [...this.hotels].filter(
-        x => x.name.toLowerCase()
-        .includes(term.toLowerCase())
-      )
-  this.setState({hotels})
-}
 
-componentDidMount(){
-  console.log('didMoint')
-  setTimeout(() =>{
-    this.setState({hotels: this.hotels, loading:false})
-  },1000)
-}
+  const changeTheme = () => {
+    const newColorTheme = (theme === 'primary') 
+    ? 'secondary'
+    : 'primary'
+    setTheme(newColorTheme)
+  }
 
-changeTheme = () => {
-  const newColorTheme = (this.state.colorTheme === 'primary') 
-  ? 'secondary'
-  : 'primary'
-  this.setState({colorTheme:newColorTheme})
-}
+  useEffect(() => {
+    setTimeout(() =>{
+      setHotels(hotelsList)
+      setLoading(false)
+    },1000)
+  }, [])
 
-  render(){
-    const header = (
-      <Header>
-        <Searchbar 
-          onSearch={(term) => this.searchHandler(term)}
-        
-        ></Searchbar>
-        <ThemeButton />
-      </Header>
-    )
-    const content = (
-      this.state.loading 
-      ? (<Loader text={'Loading Hotels'} />)
-      : (<Hotels 
-        hotels={this.state.hotels} 
-        loading={this.state.loading}
-        />)
-    )
+  const header = (
+    <Header>
+      <Searchbar 
+        onSearch={(term) => searchHandler(term)}
+      
+      ></Searchbar>
+      <ThemeButton />
+    </Header>
+  )
+  const content = (
+    loading 
+    ? (<Loader text={'Loading Hotels'} />)
+    : (<Hotels 
+      hotels={hotels} 
+      loading={loading}
+      />)
+  )
 
-    return (
-      <AuthContext.Provider value={{
-        isAuthenticated: this.state.isAuthenticated,
-        login: () => this.setState({isAuthenticated:true}),
-        logout: () => this.setState({isAuthenticated:false}),
-       }}>
-      <ThemeContext.Provider value={{
-        colorTheme: this.state.colorTheme,
-        changeTheme: this.changeTheme
-      }}>
-      <div className="App">
-        <Layout 
-          header={header}
-          menu={<Menu />}
-          content={content}
-          footer={<Footer />}
-        />
-      </div>
-      </ThemeContext.Provider>
-      </AuthContext.Provider>
-    )
-  };
+  return (
+    <AuthContext.Provider value={{
+      isAuthenticated: isAuthenticated,
+      login: () => setIsAuthenticated(true),
+      logout: () => setIsAuthenticated(false)
+     }}>
+    <ThemeContext.Provider value={{
+      colorTheme: theme,
+      changeTheme: changeTheme
+    }}>
+    <div className="App">
+      <Layout 
+        header={header}
+        menu={<Menu />}
+        content={content}
+        footer={<Footer />}
+      />
+    </div>
+    </ThemeContext.Provider>
+    </AuthContext.Provider>
+  )
 }
 
 export default App;
